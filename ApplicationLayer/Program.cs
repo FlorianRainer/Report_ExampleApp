13131767a3e2afd79e904035bdcbf869170ed5ff
Florian Rainer <florian.rainer@zeppelin-group.com>
2018-03-16 17:05:40 +0100

    Add PrivateAssets All to ProjectReference of DatabaseLayer *now compiling works like expected* *but throws System.IO.FileNotFoundException   HResult=0x80070002   Message=Could not load file or assembly 'DatabaseLayer, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null'.*

--- ApplicationLayer/Program.cs
+++ ApplicationLayer/Program.cs
@@ -8,7 +8,7 @@
         {
 	        Console.WriteLine("Hello World!");
 	        Console.WriteLine("Result: " + BusinessLayer.BusinessClass.GetSomething()); //this line is OK
-	        Console.WriteLine("Result: " + DatabaseLayer.DatabaseClass.GetSomethingFromDatabase()); //this line should not compile
+	        //Console.WriteLine("Result: " + DatabaseLayer.DatabaseClass.GetSomethingFromDatabase()); //this line should not compile
 	        Console.WriteLine("Press any key to close");
 	        Console.ReadKey();
         }
