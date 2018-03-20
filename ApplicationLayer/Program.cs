8ec4526fde3127932a7e99b1f76847f049331342
Florian Rainer <florian.rainer@zeppelin-group.com>
2018-03-20 12:43:56 +0100

    Test using <DisableTransitiveProjectReferences> inspired by https://github.com/dotnet/roslyn/blob/master/build/Targets/Imports.targets *Result, not working

--- ApplicationLayer/Program.cs
+++ ApplicationLayer/Program.cs
@@ -10,11 +10,11 @@ namespace ApplicationLayer
         {
 	        //this line is need because PrivateAssets from subprojects are not in deps.json
 	        //it is needed to add <CopyLocalLockFileAssemblies> in project to get all dlls and load it via FrameworkExtensions
-	        AssemblyLoadContext.Default.Resolving += Helper.OnAssemblyResolve;
+	        //AssemblyLoadContext.Default.Resolving += Helper.OnAssemblyResolve;
 	      
 	        Console.WriteLine("Hello World!");
 	        Console.WriteLine("Result: " + BusinessLayer.BusinessClass.GetSomething()); //this line is OK
-	        //Console.WriteLine("Result: " + DatabaseLayer.DatabaseClass.GetSomethingFromDatabase()); //this line should not compile
+	        Console.WriteLine("Result: " + DatabaseLayer.DatabaseClass.GetSomethingFromDatabase()); //this line should not compile
 	        Console.WriteLine("Press any key to close");
 	        Console.ReadKey();
         }
